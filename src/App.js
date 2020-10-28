01f19fd5eab740e391fbeda7e7ce728ffd706f50
Dean McCormack <deanmccormack@gmail.com>
2020-10-28 18:29:18 -0400

    header

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import BrokerInfo from './components/BrokerInfo';
 import ConnectionSettings from './components/ConnectionSettings';
 import GettingStartInfo from './components/GettingStartInfo';
+import Metric from './components/Metric';
 import MetricsContainer from './components/MetricsContainer';
 import RefreshRate from './components/RefreshRate';
 
@@ -64,16 +65,16 @@ const MetricUrls = [
 
 // order! Metrics=MetricsUrls
 const Metrics = {
-  [CLUSTER_SIZE]: 0,
-  [MSG_INCOMING_TOTAL_COUNT]: 0,
-  [MSG_OUTGOING_TOTAL_COUNT]: 0,
-  [MSG_QUEUED_COUNT]: 0,
-  [MSG_RETAINED_CURRENT_COUNT]: 0,
-  [NETWORKING_BYTES_READ_TOTAL]: 0,
-  [NETWORKING_BYTES_WRITE_TOTAL]: 0,
-  [NETWORKING_CONNECTIONS_CURRENT]: 0,
-  [SESSSIONS_OVERALL_COUNT]: 0,
-  [SUB_OVERALL_CURRENT_COUNT]: 0,
+  [CLUSTER_SIZE]: 4,
+  [MSG_INCOMING_TOTAL_COUNT]: 21000,
+  [MSG_OUTGOING_TOTAL_COUNT]: 17000,
+  [MSG_QUEUED_COUNT]: 5900,
+  [MSG_RETAINED_CURRENT_COUNT]: 1456,
+  [NETWORKING_BYTES_READ_TOTAL]: 219889887,
+  [NETWORKING_BYTES_WRITE_TOTAL]: 219889887,
+  [NETWORKING_CONNECTIONS_CURRENT]: 5678,
+  [SESSSIONS_OVERALL_COUNT]: 12566,
+  [SUB_OVERALL_CURRENT_COUNT]: 17566,
 };
 
 const HistoryMetrics = [];
@@ -117,10 +118,10 @@ const METRIC_REFRESH = 5000;
 function App() {
   const [metricRefresh, setMetricRefresh] = useState(METRIC_REFRESH);
   const [metrics, setMetrics] = useState(Metrics);
-  // FIXME: will grow!
   const [historyMetrics, setHistoryMetrics] = useState(HistoryMetrics);
 
   useEffect(() => {
+    /*
     const id = setInterval(async() =>  {
       const requests = MetricUrls.map(metricUrl => fetch(metricUrl));
       const responses = await Promise.all(requests);
@@ -132,12 +133,20 @@ function App() {
       setMetrics(newMetrics);
       setHistoryMetrics([...historyMetrics, newMetrics])
     }, metricRefresh);
-    return () => clearInterval(id);
+    return () => clearInterval(id);*/
   }, [historyMetrics, metricRefresh]);
 
   return (
     <div class="grid-container">
-      <header class="header">MQTT Dashboard</header>
+      <header class="header">
+        <div>LOGO</div>
+        <h1>MQTT Dashboard</h1>
+        <div>Starter launch</div>
+        <div className="cluster-info">
+          <span>{`(${MetricLabels[CLUSTER_SIZE]}: ${metrics[CLUSTER_SIZE]})`}</span>
+          <span className="status-bulb"></span>
+        </div>
+      </header>
       <aside class="side-metrics">
         <section>
           <MetricsContainer keys={ConnectionMetricsKeys} metrics={metrics} labels={MetricLabels} />
@@ -153,10 +162,10 @@ function App() {
         </section>
       </aside>
       <main class="main">
-        <p>Charts Go Here</p>
-        <p>
-          Praesent dignissim sapien id odio rutrum interdum. Phasellus feugiat blandit libero vel ultrices. Curabitur elementum placerat lectus, a faucibus quam malesuada ut. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec eget dolor euismod turpis lacinia gravida consequat a diam. Suspendisse venenatis turpis ex, eget consequat lorem tempor varius. Mauris blandit massa magna, eget vulputate urna mollis a. Vivamus ut massa ipsum. Pellentesque risus nunc, pellentesque ac euismod id, vestibulum vel risus. Fusce vitae sodales mauris. Aliquam sed arcu venenatis, pretium elit in, pretium odio. Donec interdum dignissim odio, in tempor nunc aliquet nec. Proin urna justo, pulvinar vel tellus in, ullamcorper vehicula massa.
-        </p>
+        <div id="main-chart" className="chart">Main Charts Go Here</div>
+        <div className="chart">Charts Go Here</div>
+        <div className="chart">Charts Go Here</div>
+        <div className="chart">Charts Go Here</div>
       </main>
       <aside class="info-container">
         <BrokerInfo />
